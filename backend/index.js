d288b73e20e0a082d284046cafc05d6376c720a5
andreycherepenkov <cherepenkov04@mail.ru>
2023-02-21 21:45:58 +0300

    ararar

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,34 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+
+mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.aarwuzf.mongodb.net/?retryWrites=true&w=majority',
+).then(() => console.log('DB ok'))
+.catch((err) => console.log('DB error', err));
 
 const app = express();
 
+app.use(express.json());
+
+
 app.get('/',(req,res) => {
     res.send('111hello world');
+});
+
+app.post('/auth/login',(req,res)=>{
+    console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullname: 'Вася пупкин',
+    },
+      'secret123',
+    );
+    res.json({
+        success:true,
+        token,
+    });
 });
 
 app.listen(444,(err)=>{
