ae4cb3a2e13e23b6f353bb7299bde0615b10929f
Tema <93534577+SouthKartman@users.noreply.github.com>
2023-02-22 09:30:17 +0300

    connect mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,35 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.aarwuzf.mongodb.net/?retryWrites=true&w=majority',)
+.then(() => console.log('DB connect'))
+.catch((err) => console.log('DB error', err));
+
 
 const app = express();
 
+app.use(express.json());
+
+
 app.get('/',(req,res) => {
     res.send('111hello world');
+});
+
+app.post('/auth/login',(req,res)=>{
+    console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullname: 'Вася Пупкин',
+    },
+      'secret123',
+    );
+    res.json({
+        success:true,
+        token,
+    });
 });
 
 app.listen(444,(err)=>{
@@ -13,4 +39,3 @@ app.listen(444,(err)=>{
 
     console.log('server ok')
 });
-
